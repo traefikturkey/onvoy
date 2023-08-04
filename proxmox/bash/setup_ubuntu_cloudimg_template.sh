d724471348e79a6d0c810274626d8d7f5049c3d3
Mike Glenn <219478+ilude@users.noreply.github.com>
2023-08-04 11:54:47 -0400

    Update setup_ubuntu_cloudimg_template.sh

--- proxmox/bash/setup_ubuntu_cloudimg_template.sh
+++ proxmox/bash/setup_ubuntu_cloudimg_template.sh
@@ -1,6 +1,13 @@
+#!/bin/bash 
 
+export CLOUND_INIT_USERNAME=<your_username_here>
+export CLOUND_INIT_PASSWORD=<your_password_here>
+export CLOUND_INIT_PUBLIC_KEY=$(cat ~/.ssh/authorized_keys | grep <your_email_here> )
 
 wget -nc https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
+wget -nc https://github.com/traefikturkey/onvoy/raw/master/proxmox/bash/templates/cloudinit/basic_cloudinit.yml
+envsubst < basic_cloudinit.yml > /var/lib/vz/snippets/user-data.yml
+
 # create a new VM
 qm create 9000 --memory 2048 --cores 4 --machine q35 --bios ovmf --net0 virtio,bridge=vmbr0 
 
