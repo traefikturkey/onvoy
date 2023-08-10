0ca94b302cee91fc526de1126e6cee8cd79879dc
Mike Glenn <219478+ilude@users.noreply.github.com>
2023-08-10 14:12:06 -0400

    Update purge_ceph_install.sh

--- proxmox/bash/purge_ceph_install.sh
+++ proxmox/bash/purge_ceph_install.sh
@@ -16,3 +16,4 @@ dpkg-reconfigure ceph-mds
 dpkg-reconfigure ceph-common
 dpkg-reconfigure ceph-fuse
 for i in $(apt search ceph | grep installed | awk -F/ '{print $1}'); do apt reinstall $i; done
+apt autoremove -y
