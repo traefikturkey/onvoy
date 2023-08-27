c99523d7e6871ffdc9284be92db198eda812e790
Mike Glenn <219478+ilude@users.noreply.github.com>
2023-08-27 14:41:16 -0400

    Update setup_ubuntu_cloudimg_template.sh

--- proxmox/bash/setup_ubuntu_cloudimg_template.sh
+++ proxmox/bash/setup_ubuntu_cloudimg_template.sh
@@ -2,6 +2,8 @@
 
 # curl -s https://raw.githubusercontent.com/traefikturkey/onvoy/master/proxmox/bash/setup_ubuntu_cloudimg_template.sh > setup_ubuntu_cloudimg_template.sh
 
+# qm stop 9000 --skiplock && qm destroy 9000 --destroy-unreferenced-disks --purge
+
 if [[ ! -f .env ]]; then
    echo "CLOUD_INIT_USERNAME=<your_username_here>" > .env
    echo "CLOUD_INIT_PASSWORD=<your_password_here>" >> .env
@@ -16,21 +18,14 @@ else
    eval export $(cat .env)
 fi
 
-echo "installing cloudinit and guest-agent dependencies..."
-apt update
-apt install -y cloud-init libguestfs-tools
-
 if [[ ! -f jammy-server-cloudimg-amd64.img ]]; then 
    echo "downloading cloudimg file..."
    wget -nc https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
-
-   echo "adding guest-agent to cloudimg..."
-   virt-customize --install qemu-guest-agent -a jammy-server-cloudimg-amd64.img
 fi
 
 if [[ ! -f basic_cloudinit.yml ]]; then 
    echo "downloading cloudinit file..."
-   wget -nc https://github.com/traefikturkey/onvoy/raw/master/proxmox/bash/templates/cloudinit/basic_cloudinit.yml
+   curl -s "https://raw.githubusercontent.com/traefikturkey/onvoy/master/proxmox/bash/templates/cloudinit/basic_cloudinit.yml?$(date +%s)" -o basic_cloudinit.yml
 fi
 
 echo "setup cloudinit file..."
@@ -45,13 +40,13 @@ qm importdisk $VM_ID jammy-server-cloudimg-amd64.img $VM_STORAGE > /dev/null
 
 # finally attach the new disk to the VM as scsi drive
 qm set $VM_ID --name "${VM_NAME}"
-qm set $VM_ID --scsihw virtio-scsi-pci --scsi0 $VM_STORAGE:vm-9000-disk-0
-qm set $VM_ID --ide2 $VM_STORAGE:cloudinit
-qm set $VM_ID --efidisk0 $VM_STORAGE:0,pre-enrolled-keys=1,efitype=4m
+qm set $VM_ID --scsihw virtio-scsi-pci --scsi0 $VM_STORAGE:vm-9000-disk-0,cache=writethrough,discard=on,ssd=1
+qm set $VM_ID --scsi1 $VM_STORAGE:cloudinit
+qm set $VM_ID --efidisk0 $VM_STORAGE:0,pre-enrolled-keys=1,efitype=4m,size=528K
 qm set $VM_ID --boot c --bootdisk scsi0 --ostype l26
 qm set $VM_ID --serial0 socket --vga serial0
 qm set $VM_ID --ipconfig0 ip=dhcp
-qm set $VM_ID --agent enabled=1,type=virtio --localtime 1
+qm set $VM_ID --agent enabled=1,type=virtio,fstrim_cloned_disks=1 --localtime 1
 # alternative, but the user-data.yml already has this
 #qm set $VM_ID --sshkey ~/.ssh/id_ed25519.pub
 
@@ -68,10 +63,10 @@ while [ $secs -gt 0 ]; do
    sleep 1
    : $((secs--))
 done
-
 echo "initial setup complete..."
+
+echo "shutting down and converting to template VM..."
 qm shutdown $VM_ID
 qm stop $VM_ID
 qm template $VM_ID
-
-
+echo "Operations Completed!"
