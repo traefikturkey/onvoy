7bb963a96f81af55ee483d52181bf5a0cfed4fa1
Mike Glenn <219478+ilude@users.noreply.github.com>
2023-08-29 12:05:51 -0400

    Update setup_ubuntu_cloudimg_template.sh

--- proxmox/bash/setup_ubuntu_cloudimg_template.sh
+++ proxmox/bash/setup_ubuntu_cloudimg_template.sh
@@ -73,8 +73,8 @@ while [[ "$BOOT_COMPLETE" -ne "1" ]]; do
    sleep 5
    BOOT_COMPLETE=$(qm guest exec $VM_ID -- /bin/bash -c 'ls /var/lib/cloud/instance/boot-finished | wc -l | tr -d "\n"' | jq -r '."out-data"')
 done
-echo "Cloud-init completed! Running linux sysprep..."
-qm guest exec $VM_ID -- /bin/bash -c 'echo -n >/etc/machine-id && rm /var/lib/dbus/machine-id && ln -s /etc/machine-id  /var/lib/dbus/machine-id' | jq -r '."exitcode"'
+echo "Cloud-init of template completed, replacing cloud-init user-data.yml..."
+#qm guest exec $VM_ID -- /bin/bash -c 'echo -n >/etc/machine-id && rm /var/lib/dbus/machine-id && ln -s /etc/machine-id  /var/lib/dbus/machine-id' | jq -r '."exitcode"'
 qm set $VM_ID --cicustom "user=local:snippets/clone-user-data.yml" # qm cloudinit dump 9000 user
 
 echo "shutting down and converting to template VM..."
