9776ccb6c143d890b567b92bb3cd53bf285d91c2
Mike Glenn <219478+ilude@users.noreply.github.com>
2023-09-11 08:08:01 -0700

    fix pathing for ssh key

--- proxmox/scripts/setup_ubuntu_cloudimg_template.sh
+++ proxmox/scripts/setup_ubuntu_cloudimg_template.sh
@@ -7,7 +7,7 @@
 if [[ ! -f .cloudimage.env ]]; then
    echo 'CLOUD_INIT_USERNAME=${CLOUD_INIT_USERNAME:-anvil}' > .cloudimage.env
    echo 'CLOUD_INIT_PASSWORD=${CLOUD_INIT_PASSWORD:-super_password}' >> .cloudimage.env
-   echo 'CLOUD_INIT_PUBLIC_KEY=$(cat .ssh/id_ed25519.pub)' >> .cloudimage.env
+   echo 'CLOUD_INIT_PUBLIC_KEY=$(cat ~/.ssh/id_ed25519.pub)' >> .cloudimage.env
    echo 'VM_ID=${VM_ID:-9000}' >> .cloudimage.env
    echo 'VM_STORAGE=${VM_STORAGE:-local-lvm}' >> .cloudimage.env
    echo 'VM_NAME=${VM_NAME:-ubuntu-server-22.04-template}' >> .cloudimage.env
