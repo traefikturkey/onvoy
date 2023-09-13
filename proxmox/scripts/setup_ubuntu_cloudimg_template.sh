ea762caebaaf86fc9e38a675cd5ba3ad38185635
Mike Glenn <219478+ilude@users.noreply.github.com>
2023-09-13 14:04:13 -0400

    Update setup_ubuntu_cloudimg_template.sh

--- proxmox/scripts/setup_ubuntu_cloudimg_template.sh
+++ proxmox/scripts/setup_ubuntu_cloudimg_template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-# https://raw.githubusercontent.com/traefikturkey/onvoy/master/proxmox/scripts/setup_ubuntu_cloudimg_template.sh?$(date +%s)" | /bin/bash -s
+# curl -s "https://raw.githubusercontent.com/traefikturkey/onvoy/master/proxmox/scripts/setup_ubuntu_cloudimg_template.sh?$(date +%s)" | /bin/bash -s
 
 # qm stop 9000 --skiplock && qm destroy 9000 --destroy-unreferenced-disks --purge
 
