d7e1467754a06dbf2c9785a340b4019a503a2498
Mike Glenn <219478+ilude@users.noreply.github.com>
2024-03-16 15:02:13 -0400

    Create add_github_ssh_keys.sh

---
+++ proxmox/scripts/add_github_ssh_keys.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# curl -s  | bash -s <your_github_username>
+
+if [ "$#" -ne 1 ]; then
+    echo "Please provide a github username."
+    exit 1
+fi
+
+keys=$(wget -qO- https://github.com/$1.keys)
+echo "$keys" | while read -r key
+do
+    if [ -f "~/.ssh/authorized_keys" ] && ! grep "$key" "~/.ssh/authorized_keys" &> /dev/null
+    then
+        echo "$key" >> "~/.ssh/authorized_keys"
+    fi
+done
