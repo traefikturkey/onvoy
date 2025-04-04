6fbf594a20ee85b5e205a644c006ec7123daa607
Mike Glenn <219478+ilude@users.noreply.github.com>
2025-04-04 13:46:04 -0400

    Update base_server_setup.sh

--- ubuntu/bash/base_server_setup.sh
+++ ubuntu/bash/base_server_setup.sh
@@ -65,6 +65,8 @@ sudo sysctl net.ipv6.conf.all.disable_ipv6=1
 sudo sysctl net.ipv6.conf.default.disable_ipv6=1
 echo "net.ipv6.conf.all.disable_ipv6=1" | sudo tee --append /etc/sysctl.conf
 echo "net.ipv6.conf.default.disable_ipv6=1" | sudo tee --append /etc/sysctl.conf
+# reload kernel parameters
+sudo sysctl -p
 
 # basic system administration packages
 sudo apt-get update
